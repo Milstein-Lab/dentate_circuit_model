ba7c10dd3ed0daacc85d78d4420e08c4912814f7
Aaron D. Milstein <neurosutras@gmail.com>
2021-11-11 19:06:33 -0500

    20211111: filter_features_multiple_instances:  - Increasing maximum weight bounds from 2 to 3.  - Reducing threshold criterion for fraction_active_patterns of each population.  - Running optimization that evaluates multiple seeds/instances.

--- jobscripts/optimize_dentate_circuit_model_multiple_seeds_frontera.sh
+++ jobscripts/optimize_dentate_circuit_model_multiple_seeds_frontera.sh
@@ -21,6 +21,6 @@ set -x
 cd $WORK2/dentate_circuit_model
 
 ibrun -n 1001 python3 -m nested.optimize --config-file-path=$CONFIG_FILE_PATH \
-  --output-dir=$SCRATCH/data/optimize_dentate_circuit_model --pop_size=200 --max_iter=1 --path_length=1 --disp \
+  --output-dir=$SCRATCH/data/optimize_dentate_circuit_model --pop_size=200 --max_iter=50 --path_length=3 --disp \
   --label=$LABEL --seed=$SEED
 EOT
