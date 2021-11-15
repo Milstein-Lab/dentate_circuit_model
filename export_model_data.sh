19e68940d74fd9c6eea3f7147d12397ad5cb9f9b
Aaron D. Milstein <neurosutras@gmail.com>
2021-11-15 09:52:16 -0500

    20211115:  - Edited export_model_data.sh  - fraction_active_patterns_threshold and fraction_active_units_threshold can be specified per population

--- export_model_data.sh
+++ export_model_data.sh
@@ -20,12 +20,12 @@ for ((i=0; i<${arraylength}; i++))
 do
   mpirun -n 6 python -m mpi4py.futures -m nested.analyze --config-file-path=config/${config_file_names[$i]} \
     --framework=mpi --export --export-file-path=${EXPORT_FILE_PATH_BASE}.hdf5 --param-file-path=$PARAM_FILE_PATH \
-    --model-key=${model_keys[$i]}
+    --model-key=${model_keys[$i]} --allow_fail=False
 
 done
 
 mpirun -n 6 python -m mpi4py.futures -m nested.analyze \
   --config-file-path=config/optimize_config_6b_FF_Inh+indirect_FB_Inh+FB_Exc_multiple_seeds.yaml \
   --param-file-path=$PARAM_FILE_PATH --model-key=6b --framework=mpi --export --export_dynamics \
-  --export-file-path=${EXPORT_FILE_PATH_BASE}_dynamics.hdf5
+  --export-file-path=${EXPORT_FILE_PATH_BASE}_dynamics.hdf5 --allow_fail=False
 
