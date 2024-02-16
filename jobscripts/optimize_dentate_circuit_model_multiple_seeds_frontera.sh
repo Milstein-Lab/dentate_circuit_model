08f6f86135613499743a5b9b6b31e81bfa16314b
Aaron D. Milstein <neurosutras@gmail.com>
2024-02-16 16:06:26 -0500

    20240216:  - Preparing to optimize new learning configs.

--- jobscripts/optimize_dentate_circuit_model_multiple_seeds_frontera.sh
+++ jobscripts/optimize_dentate_circuit_model_multiple_seeds_frontera.sh
@@ -12,15 +12,15 @@ sbatch <<EOT
 #SBATCH -p normal
 #SBATCH -N 18
 #SBATCH -n 1008
-#SBATCH -t 18:00:00
-#SBATCH --mail-user=neurosutras@gmail.com
-#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH -t 48:00:00
+#SBATCH --mail-user=milstein@cabm.rutgers.edu
+#SBATCH --mail-type=ALL
 
 set -x
 
 cd $WORK2/dentate_circuit_model
 
-ibrun -n 1000 python3 -m nested.optimize --config-file-path=$CONFIG_FILE_PATH \
+ibrun -n 1001 python3 -m mpi4py.futures -m nested.optimize --config-file-path=$CONFIG_FILE_PATH \
   --output-dir=$SCRATCH/data/optimize_dentate_circuit_model --pop_size=200 --max_iter=50 --path_length=3 --disp \
-  --label=$LABEL --opt_rand_seed=$SEED --framework=pc
+  --label=$LABEL --opt_rand_seed=$SEED --framework=mpi
 EOT
